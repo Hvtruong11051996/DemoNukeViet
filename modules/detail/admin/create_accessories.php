9af57b1d9b2c783aa1891e4481c67f5416eb434c
Hoang Van Truong <hoangtruongttat96@gmail.com>
2020-11-24 17:08:21 +0700

    Insert accessories_detail

--- modules/detail/admin/create_accessories.php
+++ modules/detail/admin/create_accessories.php
@@ -16,6 +16,51 @@ $page_title = $lang_module['create_accessories'];
 
 //------------------------------
 // Viết code xử lý chung vào đây
+
+$post = [];
+$error = [];
+$post['id'] = $nv_Request->get_int('id', 'post,get', 0);
+$post['accessories_id'] = $nv_Request->get_title('accessories_id', 'post', '');
+$post['slug'] = $nv_Request->get_title('slug', 'post', '');
+$post['description'] = $nv_Request->get_textarea('description', 'post', '', NV_ALLOWED_HTML_TAGS);
+$post['submit'] = $nv_Request->get_title('submit', 'post', '');
+
+if (!empty($post['submit'])) {
+
+    if (empty($error)) {
+        if ($post['id'] > 0) {
+            // update
+            $sql = "UPDATE `shop_accessories_detail` SET
+            accessories_id=:accessories_id,slug=:slug,description=:description WHERE id = " . $post['id'];
+            $s = $db->prepare($sql);
+        } else {
+            //  Insert
+            $sql = "INSERT INTO `shop_accessories_detail`
+            (`accessories_id`, `slug`,`description`, `weight`) 
+            VALUES (:accessories_id, :slug, :description, :weight)";
+            $s = $db->prepare($sql);
+            $s->bindValue('weight', 1);
+        }
+        $s->bindParam('accessories_id', $post['accessories_id']);
+        $s->bindParam('slug', $post['slug']);
+        $s->bindParam('description', $post['description']);
+        $exe = $s->execute();
+
+        if ($post['id'] > 0) {
+            $error[] = "Update ok!";
+        } else {
+            $error[] = "Insert ok !";
+        }
+    }
+}
+
+// ============= Edit dữ liệu ============ //
+if ($post['id'] > 0) {
+
+    $sql = "SELECT * FROM `shop_accessories_detail` WHERE id = "  . $post['id'];
+    $post =  $db->query($sql)->fetch();
+}
+
 //------------------------------
 
 $xtpl = new XTemplate('create_accessories.tpl', NV_ROOTDIR . '/themes/' . $global_config['module_theme'] . '/modules/' . $module_file);
@@ -27,10 +72,17 @@ $xtpl->assign('NV_NAME_VARIABLE', NV_NAME_VARIABLE);
 $xtpl->assign('NV_OP_VARIABLE', NV_OP_VARIABLE);
 $xtpl->assign('MODULE_NAME', $module_name);
 $xtpl->assign('OP', $op);
+$xtpl->assign('Error', implode('<br>', $error));
+$xtpl->assign('POST', $post);
+
 
 //-------------------------------
 // Viết code xuất ra site vào đây
 //-------------------------------
+
+if (!empty($error)) {
+    $xtpl->parse('main.error');
+}
 
 $xtpl->parse('main');
 $contents = $xtpl->text('main');
--- themes/admin_default/modules/detail/create_accessories.tpl
+++ themes/admin_default/modules/detail/create_accessories.tpl
@@ -1,21 +1,28 @@
 <!-- BEGIN: main -->
+<!-- BEGIN: error -->
+<div class='alert alert-warning' role="alert">{Error}</div>
+<!-- END: error -->
 <form
     action="{NV_BASE_ADMINURL}index.php?{NV_LANG_VARIABLE}={NV_LANG_DATA}&amp;{NV_NAME_VARIABLE}={MODULE_NAME}&amp;{NV_OP_VARIABLE}={OP}"
     method="post" enctype='multipart/form-data'>
-    <input type="hidden" class="form-control" name="student_id" value="">
+    <input type="hidden" class="form-control" name="id" value="">
 
     <div class="container">
         <div class="form-group">
             <label for=""><strong>Tên Phụ kiện : </strong> </label>
-            <input type="text" class="form-control" name="student_name" value="">
+            <input type="text" class="form-control" name="accessories_id" value="">
         </div>
         <div class="form-group">
+            <label for=""><strong>Slug : </strong> </label>
+            <input type="text" class="form-control" name="slug" value="">
+        </div>
+        <!-- <div class="form-group">
             <label><strong>Image : </strong></label>
             <input type="file" name="avatar">
-        </div>
+        </div> -->
         <div class="form-group">
             <label for=""><strong>Mô tả : </strong></label>
-            <textarea class="form-control" rows="3"></textarea>
+            <textarea name="description" class="form-control" rows="3"></textarea>
         </div>
 
         <div class="text-center">
